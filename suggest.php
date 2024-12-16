71697097f56a79d949408675293fcd3d0a4d390d
Linh <22520780@gm.uit.edu.vn>
2024-12-16 20:17:24 +0700

    chỉnh lại câu lệnh SQL

--- suggest.php
+++ suggest.php
@@ -7,9 +7,18 @@ $suggestions = [];
 
 if (!empty($q)) {
     // Sử dụng prepared statement để tránh SQL Injection
-    $stmt = $conn->prepare("SELECT productName FROM tbl_cart WHERE productName LIKE ? LIMIT 5");
+    $stmt = $conn->prepare("SELECT productName 
+    FROM tbl_product 
+    JOIN tbl_category ON tbl_product.catID = tbl_category.catID
+    JOIN tbl_brand ON tbl_product.brandID = tbl_brand.brandID
+    JOIN tbl_measure ON tbl_product.productID = tbl_measure.productID
+    WHERE tbl_product.productName LIKE ? 
+    or tbl_category.catName LIKE ? 
+    or tbl_brand.brandName LIKE ? 
+    or tbl_measure.measureName LIKE ? LIMIT 5
+    ");
     $searchTerm = "%$q%";
-    $stmt->bind_param("s", $searchTerm);
+    $stmt->bind_param("ssss", $searchTerm, $searchTerm, $searchTerm, $searchTerm);
     $stmt->execute();
     $result = $stmt->get_result();
 
